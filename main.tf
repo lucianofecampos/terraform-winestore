10c71f2474d8d0e75ec61344fc5167e193eb91ce
Luciano Ferreira Campos <luciano.fecampos@gmail.com>
2018-08-25 11:54:39 -0300

    moved folders

--- main.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  version                 = "~> 1.14"
-  shared_credentials_file = "˜/.aws/credentials"
-  profile                 = "terraform"
-  region                  = "us-east-1"
-}
--- terraform.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "htecweb-beestore"
-    key    = "beerstore-infra"
-    region = "us-east-1"
-  }
-}
