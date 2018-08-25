895a9021c36f9da2019339f6d7748650b5460817
Luciano Ferreira Campos <luciano.fecampos@gmail.com>
2018-08-25 11:57:02 -0300

    moved folders

---
+++ terraform/main.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  version                 = "~> 1.14"
+  shared_credentials_file = "˜/.aws/credentials"
+  profile                 = "terraform"
+  region                  = "us-east-1"
+}
---
+++ terraform/secutiry.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "allow_ssh" {
+  vpc_id      = "${aws_vpc.main.id}"
+  name        = "hibicode_allow_ssh"
+  description = "Security Group that allows ssh"
+
+  ingress {
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+    cidr_blocks = ["${var.my_public_cidr}"]
+  }
+
+  tags {
+    Name = "allow_ssh"
+  }
+}
+
+resource "aws_security_group" "allow_db_connections" {
+  vpc_id      = "${aws_vpc.main.id}"
+  name        = "hibicode_allow_db_connection"
+  description = "Security Group that allows app network connect to database"
+
+  ingress {
+    from_port   = 5432
+    protocol    = "tcp"
+    to_port     = 5432
+    cidr_blocks = ["${flatten(chunklist(aws_subnet.public_subnet.*.cidr_block, 1))}"]
+  }
+
+  tags {
+    Name = "allow_db_connection"
+  }
+}
